dfb242f84560f38e78b607974b2a2f38361fb242
liuxueyang <zypz457@sina.com>
2017-04-29 02:50:40 +0800

    Change Gmail port.

--- config.py
+++ config.py
@@ -22,7 +22,8 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     MAIL_SERVER = 'smtp.sina.com'
-    MAIL_PORT = 25
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get(
