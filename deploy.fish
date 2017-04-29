086c7bb20123ca0400ca8961cb3e853e2ec28147
liuxueyang <zypz457@sina.com>
2017-04-29 22:07:40 +0800

    disable reset_db in deploy.fish

--- deploy.fish
+++ deploy.fish
@@ -5,6 +5,6 @@
 #proxychains4 heroku config:set MAIL_PASSWORD=$MAIL_PASSWORD
 
 proxychains4 git push heroku master
-proxychains4 heroku run python manage.py reset_db
+#proxychains4 heroku run python manage.py reset_db
 
 proxychains4 heroku restart
