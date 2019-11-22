0d99dab75df1dbbadb31278d3b351a64ca3556bd
liuxueyang <liuxueyang457@gmail.com>
2019-11-22 20:43:25 +0800

    修复名字覆盖的一些警告，删除无用的注释

--- app/main/views.py
+++ app/main/views.py
@@ -8,55 +8,16 @@ from .forms import EditProfileForm, EditProfileAdminForm, PostForm, CommentForm
 from ..models import User, Role, Post, Permission, Comment
 from ..decorators import admin_required
 
-# This implementation is for test and exercise. Leave it alone.
-# @main.route('/', methods=['GET', 'POST'])
-# def index():
-#     form = NameForm()
-
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username=form.name.data).first()
-
-#         if user is None:
-#             user = User(username=form.name.data)
-#             db.session.add(user)
-#             session['known'] = False
-#             app = current_app._get_current_object()
-
-#             if app.config['FLASKY_ADMIN']:
-#                 send_email(
-#                     app.config['FLASKY_ADMIN'],
-#                     'New User',
-#                     'mail/new_user',
-#                     user=user)
-#         else:
-#             session['known'] = True
-
-#         old_name = session.get('name')
-
-#         if old_name is not None and old_name != form.name.data:
-#             flash('Looks like you have changed your name!')
-
-#         session['name'] = form.name.data
-#         form.name.data = ''
-#         return redirect(url_for('.index'))
-
-#     return render_template(
-#         'index.html',
-#         current_time=datetime.utcnow(),
-#         name=session.get('name'),
-#         form=form,
-#         known=session.get('known', False))
-
 
 @main.route('/user/<username>')
 def user(username):
-    user = User.query.filter_by(username=username).first()
+    _user = User.query.filter_by(username=username).first()
 
-    if not user:
+    if not _user:
         abort(404)
 
-    posts = user.posts.order_by(Post.timestamp.desc()).all()
-    return render_template('user.html', user=user, posts=posts)
+    posts = _user.posts.order_by(Post.timestamp.desc()).all()
+    return render_template('user.html', user=_user, posts=posts)
 
 
 @main.route('/edit_profile', methods=['GET', 'POST'])
@@ -79,32 +40,32 @@ def edit_profile():
     return render_template('edit_profile.html', form=form)
 
 
-@main.route('/edit_profile/<int:id>', methods=['GET', 'POST'])
+@main.route('/edit_profile/<int:_id>', methods=['GET', 'POST'])
 @login_required
 @admin_required
-def edit_profile_admin(id):
-    user = User.query.get_or_404(id)
-    form = EditProfileAdminForm(user=user)
+def edit_profile_admin(_id):
+    _user = User.query.get_or_404(_id)
+    form = EditProfileAdminForm(user=_user)
 
     if form.validate_on_submit():
-        user.email = form.email.data
-        user.username = form.username.data
-        user.confirmed = form.confirmed.data
-        user.role = Role.query.get(form.role.data)
-        user.name = form.name.data
-        user.location = form.location.data
-        user.about_me = form.about_me.data
-        db.session.add(user)
+        _user.email = form.email.data
+        _user.username = form.username.data
+        _user.confirmed = form.confirmed.data
+        _user.role = Role.query.get(form.role.data)
+        _user.name = form.name.data
+        _user.location = form.location.data
+        _user.about_me = form.about_me.data
+        db.session.add(_user)
         flash('The profile has been updated.')
-        return redirect(url_for('.user', username=user.username))
+        return redirect(url_for('.user', username=_user.username))
 
-    form.email.data = user.email
-    form.username.data = user.username
-    form.confirmed.data = user.confirmed
-    form.role.data = user.role_id
-    form.name.data = user.name
-    form.location.data = user.location
-    form.about_me.data = user.about_me
+    form.email.data = _user.email
+    form.username.data = _user.username
+    form.confirmed.data = _user.confirmed
+    form.role.data = _user.role_id
+    form.name.data = _user.name
+    form.location.data = _user.location
+    form.about_me.data = _user.about_me
 
     return render_template('edit_profile.html', form=form)
 
@@ -115,9 +76,9 @@ def index():
 
     if current_user.can(
             Permission.WRITE_ARTICLES) and form.validate_on_submit():
-        post = Post(
+        _post = Post(
             body=form.body.data, author=current_user._get_current_object())
-        db.session.add(post)
+        db.session.add(_post)
         return redirect(url_for('.index'))
 
     page = request.args.get('page', 1, type=int)
@@ -130,27 +91,27 @@ def index():
         'index.html', form=form, posts=posts, pagination=pagination)
 
 
-@main.route('/post/<int:id>', methods=['GET', 'POST'])
-def post(id):
-    post = Post.query.get_or_404(id)
+@main.route('/post/<_id>', methods=['GET', 'POST'])
+def post(_id):
+    _post = Post.query.get_or_404(_id)
     form = CommentForm()
 
     if form.validate_on_submit():
         comment = Comment(
             body=form.body.data,
-            post=post,
+            post=_post,
             author=current_user._get_current_object())
         db.session.add(comment)
         flash('评论发表成功啦～')
-        return redirect(url_for('.post', id=post.id, page=-1))
+        return redirect(url_for('.post', id=_post.id, page=-1))
 
     page = request.args.get('page', 1, type=int)
 
     if -1 == page:
-        page = (post.comments.count() - 1
+        page = (_post.comments.count() - 1
                 ) / current_app.config['MINIBLOG_COMMENTS_PER_PAGE'] + 1
 
-    pagination = post.comments.order_by(Comment.timestamp.asc()).paginate(
+    pagination = _post.comments.order_by(Comment.timestamp.asc()).paginate(
         page,
         per_page=current_app.config['MINIBLOG_COMMENTS_PER_PAGE'],
         error_out=False)
@@ -158,16 +119,16 @@ def post(id):
 
     return render_template(
         'post.html',
-        posts=[post],
+        posts=[_post],
         form=form,
         comments=comments,
         pagination=pagination)
 
 
-@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@main.route('/edit/<int:_id>', methods=['GET', 'POST'])
 @login_required
-def edit(id):
-    post = Post.query.get_or_404(id)
+def edit(_id):
+    post = Post.query.get_or_404(_id)
 
     if current_user != post.author and \
        not current_user.can(Permission.ADMINISTER):
